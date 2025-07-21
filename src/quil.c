8524ccb9a857174ddb1bb99df5e02a780a5a7429
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2025-07-21 12:07:46 -0500

    initial commit

---
+++ src/quil.c
@@ -0,0 +1,9 @@
+//
+// Created by Osprey on 7/21/2025.
+//
+
+
+
+void QUIL_initializeGLFW()
+{
+}
---
+++ src/quil.h
@@ -0,0 +1,18 @@
+//
+// Created by Osprey on 7/21/2025.
+//
+
+#pragma once
+
+#ifndef QUIL_H
+#define QUIL_H
+
+#endif //QUIL_H
+
+void QUIL_initializeGLFW();
+
+void QUIL_initializeInputStates();
+
+void QUIL_pollGLFWInput();
+
+void QUIL_terminateGLFW();
